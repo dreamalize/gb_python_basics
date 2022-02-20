f4dd1c1accaefc857da382dca314cfbcdc0b9f1d
dreamalize <dreamalize1@gmail.com>
2022-02-21 01:30:05 +0300

    Initial commit

---
+++ 1-lesson/1-lesson_task_1.py
@@ -0,0 +1,17 @@
+title = "Анкета пользователя"
+
+print(title)
+print("---")
+
+user_name = input("Введите Ваше Имя: ")
+user_lastname = input("Введите Вашу Фамилию: ")
+user_age = int(input("Ввеите Ваш возраст: "))
+user_city = input("Из какого Вы города: ")
+user_phone = input("Введите ваше телефон: ")
+print()
+print("--- Спасибо! Ваши данные записаны ---")
+print(user_name, user_lastname)
+print(f"Возраст: {user_age}")
+print("Город: {}, Телефон: {}".format(user_city, user_phone))
+
+
