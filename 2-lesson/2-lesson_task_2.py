64c3caae09fb30fb73581c5cf789800b373a213b
dreamalize <dreamalize1@gmail.com>
2022-02-28 01:39:45 +0300

    приведены к стандартам PEP

--- 2-lesson/2-lesson_task_2.py
+++ 2-lesson/2-lesson_task_2.py
@@ -1,9 +1,8 @@
 user_input = input("Введите значения для списк через пробел >>> ")
 my_list = user_input.split()
-for i in range(0, len(my_list)-1, 2):
-    my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+for i in range(0, len(my_list) - 1, 2):
+    my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
 print(f"Результат: {my_list}")
-
 
 """Альтернативный вариант через цикл while
 смена происходит после каждого ввода занчеия пользователем"""
--- 2-lesson/2-lesson_task_4.py
+++ 2-lesson/2-lesson_task_4.py
@@ -1,4 +1,4 @@
 user_input = input("Введите список слов через пробел >>> ")
 user_input = user_input.split()
 for i in range(len(user_input)):
-    print(f"{i+1}.", user_input[i][:10])
+    print(f"{i + 1}.", user_input[i][:10])
